45152780e1ff422627216cc4e3564e0b7d3237b9
Margarita Popova <nitritka93@gmail.com>
2025-05-04 15:39:48 +0300

    created basic arch

--- cmd/notes/main.go
+++ cmd/notes/main.go
@@ -3,5 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+	//TODO: Сделать конфигурацию
+	panic(fmt.Errorf("not implemented"))
 }
---
+++ internal/repository/in-memory/in-memory.go
@@ -0,0 +1,4 @@
+package inmemory
+
+type InMemory struct {
+}
