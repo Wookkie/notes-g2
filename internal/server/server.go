b6cdad3e45cc47753985b303bfd7af7d1874e566
Margarita Popova <nitritka93@gmail.com>
2025-05-06 18:12:01 +0300

    add server init

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,55 @@
 package server
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	cfg       any
+	httpServe *http.Server
+}
+
+func New(host string, port string) *Server {
+	httpServe := http.Server{
+		Addr: host + ":" + port,
+	}
+
+	router := gin.Default()
+	configRoutes(router)
+
+	httpServe.Handler = router
+
+	return &Server{
+		httpServe: &httpServe,
+	}
+}
+
+func configRoutes(router *gin.Engine) {
+	router.GET("/")
+	users := router.Group("/users")
+	{
+		users.GET("/porfile")
+		users.POST("/register")
+		users.POST("/login")
+	}
+
+	notes := router.Group("/notes")
+	{
+		notes.GET("/")
+		notes.GET("/:id")
+		notes.POST("/")
+		notes.PUT("/:id")
+		notes.DELETE("/:id")
+	}
+}
+
+func (s *Server) Run() error {
+	return s.httpServe.ListenAndServe()
+}
+
+func (s *Server) Stop(ctx context.Context) error {
+	return s.httpServe.Shutdown(ctx)
+}
